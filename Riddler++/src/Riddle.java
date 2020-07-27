878439f4488128498e2185584ef3764f381a75a5
taramla <tla2@luc.edu>
2020-07-27 13:18:39 -0500

    version 10 Riddle class

--- Riddler++/src/Riddle.java
+++ Riddler++/src/Riddle.java
@@ -1,16 +1,22 @@
 public class Riddle {
-    public Riddle(){
-        String riddleQuestion;
-        String riddleAnswer;
-        String riddleHint;
-        String difficulty;
-        int value;
+    public String riddleQuestion;
+    private String riddleAnswer;
+    private String riddleHint;
+    public String difficulty;
+    public int value;
+
+    public Riddle(String riddleQuestion, String riddleAnswer, String riddleHint, String difficulty, int value ){
+        this.riddleQuestion = riddleQuestion;
+        this.riddleAnswer = riddleAnswer;
+        this.riddleHint = riddleHint;
+        this.difficulty = difficulty;
+        this.value = value;
     }
     public boolean checkRiddleAnswer(String answer){
-        if(answer.equals(this.riddleAnswer)) return true;
+        if(answer.equals(riddleAnswer)) return true;
         else return false;
     }
     public String findRiddleHint(){
-        return this.riddleHint;
+        return riddleHint;
     }
 }
