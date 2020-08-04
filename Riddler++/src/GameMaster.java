a0d57e6d1d241d064823ca17d4e84425c09280cd
William Honig <whonig@luc.edu>
2020-08-03 21:15:08 -0500

    GameMaster, no code changes, inline code reading comments through middle of playround, current line 59

--- Riddler++/src/GameMaster.java
+++ Riddler++/src/GameMaster.java
@@ -4,6 +4,7 @@ public class GameMaster<fileName> {
      * GameMaster Stub
      * 	 * @author kaylinluck
      * 	 * @version 10
+     * 	 * @version 10.1 DH comments from code reading inline, marked //dHn for referenc
      *
      *   A nearly complete version of GM
      */
@@ -23,6 +24,8 @@ public class GameMaster<fileName> {
 
     public GameMaster(String fileName) throws Exception { //unfinished
         //need a method in Player like askPlayerInfo(): String to get player name, etc.
+        //dH1: yes, but it need not be a public method, we show in class diagram that Player has it in an attribute and can use it; GM does not need to know name
+        //dH2: see style guide A1.6.4, never throw "Exception", but a more specific one, here likely file problem passes up from RiddleDataBase to GameMaster to main (for now)
         myRDB = new RiddleDataBase(fileName);
         unusedListOfRiddles = myRDB.getAllRiddles();
     }
@@ -31,20 +34,29 @@ public class GameMaster<fileName> {
     public boolean playRound() {
         while(!response.equals("quit") || roundOver == true) {
             String setUp = myPlayer.askRoundSetUp();
+            //dH3: I despise declarations and assignments on the same line; strongly urge a cast of characters at the start of the method for local varialbes
+            //dh3: here you will also declare a new String object each time through the loop which is not necessary and inefficient
+            //dh4: UML shows two values returned from askRoundSetup, we should update the UML to show it's all one String with "difficulty,numberToPlay"
             String[] s = setUp.split(",");
+            //dH5: check size of s, must be 2 or throw exception (I would suggest a postcondition on askRoundSetUp to ensure length two instead; then don't need to check here)
             difficulty = s[0];
             numberToPlay = Integer.parseInt(s[1]);
             riddlesForRound = makeRiddleList(difficulty, numberToPlay);
+            //dH6: nice helper method; playRound is a bit long now, could move more out
             int c = rand.nextInt(riddlesForRound.size() - 1);
+            //dh3again:  all over again, you really don't want to declare a new c each time, right?
             currentRiddle = riddlesForRound.get(c);
             myPlayer.startTimer();
             for (int i = 0; i <= riddlesForRound.size() - 1; i++) {
+                //dH7: middle term can be replaced by numberToPlay??
                 response = myPlayer.askRiddleQuestion(currentRiddle.riddleQuestion);
                 boolean nextRiddle = false;
                 int counter = 0;
+                //dH8: counter for what, better variable name needed.  Is this same as i in the for loop?
                 while (nextRiddle != true) {
                     if (currentRiddle.checkRiddleAnswer(response) == true) {
                         riddlesForRound.remove(currentRiddle);
+                        //dH9: I am confused on the control flow here; why not remove currentRiddle right after get it?
                         if (counter == 0) {
                             myPlayer.tellCorrect(currentRiddle.value);
                         }
