58769a515c509b55c8a14e42bd18d86ce9bfe351
taramla <tla2@luc.edu>
2020-08-10 13:02:09 -0500

    version 11 riddleDataBase class with updated myRiddles text file

--- Riddler++/src/RiddleDataBase.java
+++ Riddler++/src/RiddleDataBase.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 /**
  * riddleDataBase class for Riddler++
  * @author taramla
- * @version 10
+ * @version 11
  * @version 10.1 dH comments inline, marked //dHn
  *
  * Used to create one hashmap of key (easy, medium, and hard), value (ArrayList<Riddle>)
@@ -15,16 +15,15 @@ public class RiddleDataBase {
     private HashMap<String, ArrayList<Riddle>> fullListOfRiddles;
 
     public RiddleDataBase(String fileName) throws Exception{
+        fullListOfRiddles = new HashMap<>();
         String data = "";
         ArrayList<Riddle> easyRiddles = new ArrayList<>();
         ArrayList<Riddle> medRiddles = new ArrayList<>();
         ArrayList<Riddle> hardRiddles = new ArrayList<>();
         data = new String(Files.readAllBytes(Paths.get(fileName)));
         String[] riddleArray = data.split("\n");
-        //dh4: this is really cool code; no idea if it works, but I like it; is this how you did it in Comp170??
-        for (String r :riddleArray) {
+        for (String r : riddleArray) {
             Riddle newRiddle = makeRiddle(r);
-            //dh5: here, unlike my comment below and in K's code, we really do need to make a new object each time and do need to declare in the loop
             if (newRiddle.difficulty.equals("easy")) {
                 easyRiddles.add(newRiddle);
             }
@@ -35,11 +34,21 @@ public class RiddleDataBase {
                 hardRiddles.add(newRiddle);
             }
         }
-        //dH6: I'd add a debug output here with the number of riddles of each type; maybe compare the sum to the length of riddleArray
         fullListOfRiddles.put("easy", easyRiddles);
         fullListOfRiddles.put("medium", medRiddles);
         fullListOfRiddles.put("hard", hardRiddles);
+        System.err.println("number of easy riddles: " + easyRiddles.size());
+        System.err.println("number of medium riddles: " + medRiddles.size());
+        System.err.println("number of hard riddles: " + hardRiddles.size());
+        if ((easyRiddles.size() + medRiddles.size() + hardRiddles.size()) == riddleArray.length) {
+            System.err.println("The input file matches");
+        }
+        else {
+            System.err.println("Input file mismatch. File length: " + riddleArray.length + "Sum length: " + easyRiddles.size() + medRiddles.size() + hardRiddles.size());
+        }
+
     }
+
 
     public HashMap getAllRiddles(){
         return fullListOfRiddles;
@@ -54,14 +63,13 @@ public class RiddleDataBase {
      * @param data one line of text from the Riddle file
      * @return Riddle object created from the text input
      */
-    private Riddle makeRiddle(String data) {
+    private Riddle makeRiddle(String data)  {
         String[] riddleArray = data.split("/");
-        //dH2: I hate declarations and assignments on same line; unlike similar comments in K's code, here is may be ok since it's soooo simple
         //dH3: HOWEVER, you do need to check for number you get from split; likewise some of these cannot be empty string, right? value must be valid int so you need to catch exception here
         String riddleQuestion = riddleArray[0];
         String riddleAnswer = riddleArray[1];
-        String riddleHint =riddleArray[2];
-        String difficulty = riddleArray[3];
+        String difficulty = riddleArray[2];
+        String riddleHint = riddleArray[3];
         String value = riddleArray[4];
         Riddle singleRiddle = new Riddle(riddleQuestion,riddleAnswer,riddleHint,difficulty,Integer.parseInt(value));
         return singleRiddle;
